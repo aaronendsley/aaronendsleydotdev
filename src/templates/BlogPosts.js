6b1e7b03003b1ac2d3afb38685bb2d197e4d0813
Aaron Endsley <aaronendsley@gmail.com>
2021-04-28 19:51:41 -0500

    Added Image Styling
    Added image styling with styled components

--- src/templates/BlogPosts.js
+++ src/templates/BlogPosts.js
@@ -3,6 +3,31 @@ import React from 'react';
 import BlockContent from '@sanity/block-content-to-react';
 import imageUrlBuilder from '@sanity/image-url';
 import sanityClient from '@sanity/client';
+import styled from 'styled-components';
+
+const ImageTag = styled.img`
+  width: 80%;
+  margin: 50px auto;
+  display: block;
+  border: solid 8px #25b3b8;
+  border-radius: 20px;
+  @media (min-width: 1000px) {
+    width: 400px;
+  }
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  display: block;
+`;
+
+function ImageComponent({ source }) {
+  return (
+    <ImageContainer>
+      <ImageTag src={source} alt="" />
+    </ImageContainer>
+  );
+}
 
 const client = sanityClient({
   projectId: process.env.GATSBY_SANITY_ID,
@@ -19,7 +44,7 @@ function urlFor(source) {
 const serializer = {
   types: {
     image: props => (
-      <img src={urlFor(props.node.asset._ref).width(200).url()} alt="" />
+      <ImageComponent source={urlFor(props.node.asset._ref).width(500).url()} />
     ),
   },
 };
@@ -29,7 +54,6 @@ export default function BlogPosts({ data: { blog } }) {
   return (
     <div>
       <BlockContent blocks={blog._rawPostContent} serializers={serializer} />
-      hi
     </div>
   );
 }
