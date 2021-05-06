d31870fd267ef9de094354dcd1d19303cb3da71c
Aaron Endsley <aaronendsley@gmail.com>
2021-05-05 20:40:19 -0500

    Created Portfolio Pages
    Built the function for creating Portfolio pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,8 +40,44 @@ async function turnBlogPostsIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnPostsIntoPortfolioPages({ graphql, actions }) {
+  const postsTemplate = path.resolve('./src/templates/Posts.js');
+  const { data } = await graphql(`
+    query {
+      portfolio: allSanityPortfolio {
+        nodes {
+          itemSlug {
+            current
+          }
+          itemTitle
+          _rawItemContent
+        }
+      }
+    }
+  `);
+
+  data.portfolio.nodes.forEach(portfolio => {
+    console.log(
+      `creating page for the blog post called ${portfolio.itemSlug.current}`
+    );
+    actions.createPage({
+      // what is the url for this new page
+      path: `portfolio/${portfolio.itemSlug.current}`,
+      component: postsTemplate,
+      context: {
+        slug: portfolio.itemSlug.current,
+        title: portfolio.itemTitle,
+        content: portfolio._rawItemContent,
+      },
+    });
+  });
+}
+
 exports.createPages = async function createPages(params) {
   // Create pages dynamically
   // wait for all promises to be resolved before finishing this function
-  await Promise.all([turnBlogPostsIntoPages(params)]);
+  await Promise.all([
+    turnBlogPostsIntoPages(params),
+    turnPostsIntoPortfolioPages(params),
+  ]);
 };
